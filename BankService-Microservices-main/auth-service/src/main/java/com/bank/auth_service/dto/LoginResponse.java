97a0f72e584aebae15cf6d0c8ed1887efd56a278
kamarthivani-123 <kamarthi.vani@mphasis.com>
2026-07-16 11:10:54 +0530

    Update LoginResponse.java

--- BankService-Microservices-main/auth-service/src/main/java/com/bank/auth_service/dto/LoginResponse.java
+++ BankService-Microservices-main/auth-service/src/main/java/com/bank/auth_service/dto/LoginResponse.java
@@ -3,12 +3,17 @@ package com.bank.auth_service.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
-
-@AllArgsConstructor
 public class LoginResponse {
-	private String token;
+	
+ private String token;
 
+    public LoginResponse(String token) {
+        this.token = token;
+    }
 
-	    }
+    public String getToken() {
+        return token;
+    }
+
 
 }
